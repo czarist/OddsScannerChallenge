7489bfd3c4bae45e5d7efec97b1ba6c365d3309a
Cezar Trentin <55374787+czarist@users.noreply.github.com>
2022-12-15 20:10:48 -0300

    curl method

---
+++ PHP/methods/curl.php
@@ -0,0 +1,50 @@
+<?php
+function xml_request($path)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $path);
+    curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+    $retValue = curl_exec($ch);
+    curl_close($ch);
+    return $retValue;
+}
+
+function convertToUSD($currentValue, $usdValue): float
+{
+    $convertedValue = $currentValue / $usdValue;
+    return round($convertedValue, 3);
+}
+
+$sXML = xml_request('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml?5105e8233f9433cf70ac379d6ccc5775');
+$oXML = new SimpleXMLElement($sXML);
+
+$xml = $oXML[0]->Cube->Cube;
+$xml2array = json_decode(json_encode((array)$xml), TRUE);
+$currentData = $xml2array['@attributes']['time'];
+$currencyRates = $xml2array['Cube'];
+$usdValue = floatval($currencyRates[0]["@attributes"]['rate']);
+$arrayOfCurrents = [
+    [
+        'currency' => 'EUR',
+        'rate' => convertToUSD(1, $usdValue)
+    ]
+];
+
+foreach ($currencyRates as $currencyRate) {
+    $currency = $currencyRate['@attributes']['currency'];
+    $rate = floatval($currencyRate['@attributes']['rate']);
+
+    $insert = [
+        'currency' => $currency,
+        'rate' => convertToUSD($rate, $usdValue)
+    ];
+
+    if ($currency !== 'USD') {
+        array_push($arrayOfCurrents, $insert);
+    }
+}
+
+print(json_encode($arrayOfCurrents, TRUE));
