3861ec4ed1d6f6c0439d8545ccb0d0fc1ed67217
Cezar Trentin <55374787+czarist@users.noreply.github.com>
2022-12-15 17:46:29 -0300

    strucure

---
+++ PHP/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CURL example + CSV generating</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.2.js" integrity="sha256-pkn2CUZmheSeyssYw3vMp1+xyub4m+e+QK4sQskvuo4=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="scripts.js"></script>
+</head>
+
+<header class="header">
+
+</header>
+
+<body>
+    <div class="container">
+
+    </div>
+</body>
+
+</html>
